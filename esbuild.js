8c3f1ad25017d3b2efa865675939606d7fc3d884
selemondev <selemondev19@gmail.com>
2026-05-06 14:15:57 -0600

    chore: add new extension build script and update workflows

--- esbuild.js
+++ esbuild.js
@@ -26,7 +26,7 @@ const esbuildProblemMatcherPlugin = {
 async function main() {
 	const ctx = await esbuild.context({
 		entryPoints: [
-			'src/extension.ts'
+			'./src/extension.ts'
 		],
 		bundle: true,
 		format: 'cjs',
